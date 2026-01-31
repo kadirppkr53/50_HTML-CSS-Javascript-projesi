91979faef91792319577c66019ccd3fcb29f523f
kadirppkr53 <kadirppkr53@gmail.com>
2026-01-31 16:08:46 +0300

    uygulama tamamlandı

---
+++ 10-Şans-havuzu/script.js
@@ -0,0 +1,65 @@
+const tagsEl = document.getElementById("tags")    
+const textArea = document.getElementById("textarea")
+
+textArea.focus();
+
+textArea.addEventListener("keyup", (e) => {
+    crateTags(e.target.value)
+
+    if(e.key === "Enter"){
+        setTimeout(() => {
+            e.target.value =""       
+        },10)
+        randomSelect()
+    }
+})
+
+function crateTags(input){
+    const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim())
+    console.log(tags);
+
+    tagsEl.innerHTML=""
+    tags.forEach((tag) => {
+        const tagEl = document.createElement("span")
+        tagEl.classList.add("tags")
+        tagEl.innerHTML = tag;
+        tagsEl.appendChild(tagEl) 
+    });
+}
+
+function randomSelect(){
+    const times = 30
+
+    const interval = setInterval(()=>  {
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(()=>{
+            unHighlightTag(randomTag)
+        },100)
+    },100)
+
+    setTimeout(() =>{
+        clearInterval(interval)
+        setTimeout(()=>{
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        },100)
+    }, times *100)
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll(".tags")
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+    tag.classList.add("highlight")
+}
+
+function unHighlightTag(tag){
+    tag.classList.remove("highlight")
+}
